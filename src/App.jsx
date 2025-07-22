52469d9c4fb134556dd216cb377b064def6424a9
WildoDev <wildofari99@gmail.com>
2025-07-22 11:51:48 -0300

    Initial structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-white'>
+      <h1 className='text-blue-800' >
+        Esto sera pomodora-app
+      </h1>
+    </div>
+  )
+}
+
+export default App
